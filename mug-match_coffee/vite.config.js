4dea1b92db9787a436422e7ab441f8f574f293da
ElianaScipt <eliana.liantonio@gmail.com>
2025-02-13 16:31:01 -0500

    edit vit,config.js

--- mug-match_coffee/vite.config.js
+++ mug-match_coffee/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: './',  
+  base: "/mug-match_coffee/",  
   build: {
     outDir: '../dist',
   },
